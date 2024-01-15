6469f01ac7068f9cf9017968195b9d82e757b547
Bruno Roberto <brunomoraisb2015@hotmail.com>
2024-01-15 13:07:56 -0300

    Program

--- AgendamentoHoteisService/Program.cs
+++ AgendamentoHoteisService/Program.cs
@@ -1,17 +1,14 @@
-using AgendamentoHoteis.Business.Interfaces;
-using AgendamentoHoteis.Business.Services;
 using AgendamentoHoteis.Service.Configuration;
 using AgendamentoHoteisService;
 using AgendamentoHoteisService.Data.Context;
-using AgendamentoHoteisService.Data.Repositorio;
 using Microsoft.EntityFrameworkCore;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((hostContext, services) =>
     {
         services.AddHostedService<Worker>();
-        services.AddScoped<IAgendamentoService, AgendamentoService>();
-        services.AddScoped<IAgendamentoRepositorio, AgendamentoRepositorio>();
+        services.AddServiceAplication();
+        services.AddRepository();
         IConfiguration config = hostContext.Configuration;
 
         string mySqlConnection =
@@ -20,10 +17,6 @@ IHost host = Host.CreateDefaultBuilder(args)
         services.AddDbContext<AppDbContext>(options => 
             options.UseMySql(mySqlConnection,
                       ServerVersion.AutoDetect(mySqlConnection)));
-
-        //services.AddServiceAplication();
-        //services.AddRepository();
-
     })
     .Build();
 
